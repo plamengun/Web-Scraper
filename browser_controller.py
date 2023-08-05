b4435d8c84f1b96493bf6991800cdc34f74ea00a
pgunchev <plamen.gunchev@dxc.com>
2023-08-05 22:14:27 +0300

    Login with playwright

--- browser_controller.py
+++ browser_controller.py
@@ -1,7 +1,15 @@
 from playwright.sync_api import sync_playwright
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False, slow_mo=50)
+    browser = p.chromium.launch(headless=False, slow_mo=500)
     page = browser.new_page()
-    page.goto('https://www.upwork.com/nx/find-work/')
+    page.goto('https://www.upwork.com/ab/account-security/login')
+    page.fill('input#login_username', 'plamenmgunchev44@gmail.com')
+    page.click('button#login_password_continue')
+    page.fill('input#login_password', os.environ.get('UW_PASSWORD'))
+    page.click('button#login_control_continue')
+    page.wait_for_event('pageload')
