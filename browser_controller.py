86b480be14ddfa5b522047c99e6411cb0d8039db
pgunchev <plamen.gunchev@dxc.com>
2023-08-07 18:26:39 +0300

    Logging and clicking filter done

--- browser_controller.py
+++ browser_controller.py
@@ -5,11 +5,25 @@ import os
 load_dotenv()
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False, slow_mo=500)
+    browser = p.chromium.launch(headless=False, slow_mo=50)
     page = browser.new_page()
     page.goto('https://www.upwork.com/ab/account-security/login')
     page.fill('input#login_username', 'plamenmgunchev44@gmail.com')
     page.click('button#login_password_continue')
     page.fill('input#login_password', os.environ.get('UW_PASSWORD'))
     page.click('button#login_control_continue')
-    page.wait_for_event('pageload')
+    page.wait_for_event("load")
+    url = page.url
+    anchor = page.locator(f"//div[@data-test='saved-searches']//span/a[@data-test-key='my_ideal_search']")
+    anchor.click()
+    page.wait_for_event("load")
+    dropdown = page.locator(f"//div[@data-test='jobs_per_page']//div[@data-test='dropdown-toggle']")
+    dropdown.click()
+
+    option = dropdown.locator()
+    option.click()
+    page.wait_for_event("load")
+    page.close()
+    print(url)
+
+
