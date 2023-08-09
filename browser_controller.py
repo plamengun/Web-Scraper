61edea355d1c5815a3618d187450b2ac8155d67b
pgunchev <plamen.gunchev@dxc.com>
2023-08-09 15:43:28 +0300

    Browser controller done, added storage file

--- browser_controller.py
+++ browser_controller.py
@@ -4,8 +4,9 @@ import os
 
 load_dotenv()
 
-def rss_feed_extractor(page):
+def _pw_rss_feed_extractor(page):
     n = 0
+    rss_feeds = []
     while n < 5:
         rss_feed_dropdown = page.locator(f"//div[@id='rss-atom-links']")
         rss_feed_dropdown.click()
@@ -13,30 +14,31 @@ def rss_feed_extractor(page):
         rss_feed_button.click()
         new_page = page.wait_for_event('popup')
         new_page.bring_to_front()
-        print(new_page.url)
+        rss_feeds.append(new_page.url)
         new_page.close()
         page.bring_to_front()
         page.wait_for_selector(f"//div[@class='up-card-footer pb-0 d-flex justify-space-between']//button[@class='up-pagination-item up-btn up-btn-link']", timeout=5000)
         next_button = page.locator(f"//div[@class='up-card-footer pb-0 d-flex justify-space-between']//button[@class='up-pagination-item up-btn up-btn-link']/div[@class='next-icon up-icon']")
         next_button.click()
         n += 1
+    return rss_feeds
 
 #ToDo Look into how to implement it in Headless mode
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False, slow_mo=50)
-    context = browser.new_context()
-    page = context.new_page()
-    page.goto('https://www.upwork.com/ab/account-security/login')
-    page.fill('input#login_username', 'plamenmgunchev44@gmail.com')
-    page.click('button#login_password_continue')
-    page.fill('input#login_password', os.environ.get('UW_PASSWORD'))
-    page.click('button#login_control_continue')
-    page.wait_for_event("load")
-    anchor = page.locator(f"//div[@data-test='saved-searches']//span/a[@data-test-key='my_ideal_search']")
-    anchor.click()
-    page.wait_for_event("load")
-    
-    rss_feed_extractor(page)
-
-    page.close()
+def pw_login():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False, slow_mo=50)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto('https://www.upwork.com/ab/account-security/login')
+        page.fill('input#login_username', os.environ.get('UW_EMAIL'))
+        page.click('button#login_password_continue')
+        page.fill('input#login_password', os.environ.get('UW_PASSWORD'))
+        page.click('button#login_control_continue')
+        page.wait_for_event("load")
+        anchor = page.locator(f"//div[@data-test='saved-searches']//span/a[@data-test-key='my_ideal_search']")
+        anchor.click()
+        page.wait_for_event("load")
+        rss_feeds = _pw_rss_feed_extractor(page)
+        page.close()
+    return rss_feeds
--- scraper.py
+++ scraper.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
+from browser_controller import pw_login
+from common.storage import job_dict
 
 
-resp_url = requests.get('https://www.upwork.com/ab/feed/jobs/rss?q=python&contractor_tier=2%2C3&proposals=0-4&sort=recency&paging=0%3B10&api_params=1&securityToken=f2de7592ae0640de30c3908545fdc8d53c89e8d1aabe736eac8074a2c18280dd2cee848455935a36dafa06163c4407dfe3b8936cb2699be7023843af15b0995b&userUid=703923235574398976&orgUid=703923235633119233')
+resp_url = requests.get(f"https://www.upwork.com/ab/feed/jobs/rss?q=python&contractor_tier=2%2C3&proposals=0-4&sort=recency&paging=0%3B10&api_params=1&securityToken=f2de7592ae0640de30c3908545fdc8d53c89e8d1aabe736eac8074a2c18280dd2cee848455935a36dafa06163c4407dfe3b8936cb2699be7023843af15b0995b&userUid=703923235574398976&orgUid=703923235633119233")
 
 soup = BeautifulSoup(resp_url.content, 'xml')
 entries = soup.find_all('item')
