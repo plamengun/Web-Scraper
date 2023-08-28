ad12f633662ec0c47fec0cbff86d309fabda405c
Plamen Gunchev <plamenmgunchev@gmail.com>
2023-08-28 09:57:01 +0300

    Check for questions function dev

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ load_dotenv()
 # def pw_job_posting():
 
 
-def pw_login():
+def pw_job_posting_scrape():
 
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False, slow_mo=50)
@@ -40,9 +40,44 @@ def pw_login():
 
     return description, connects_required, available_connects, posted_on_text, client_location_text
 
-print(pw_login())
+# print(pw_job_posting_scrape())
 
 
 def calculate_connects(connects_required: int = 12, available_connects: int = 1) -> str | None:
     if available_connects - connects_required >= 0:
         return 'True'
+
+
+job_application = 'https://www.upwork.com/ab/proposals/job/~0174eebd2faa73997b/apply/'
+
+
+def pw_check_for_questions():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False, slow_mo=50)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto('https://www.upwork.com/ab/account-security/login')
+        page.fill('input#login_username', os.environ.get('UW_EMAIL'))
+        page.click('button#login_password_continue')
+        page.fill('input#login_password', os.environ.get('UW_PASSWORD'))
+        page.click('button#login_control_continue')
+        page.wait_for_event("load")
+
+        page = context.new_page()
+        page.goto('https://www.upwork.com/ab/proposals/job/~01941c52781ae7d491/apply/')
+        if page.query_selector(f'//div[@class="fe-proposal-job-questions questions-area"]'):
+            questions_texts_list = []
+            questions_elements = page.query_selector_all(f'//div[@class="fe-proposal-job-questions questions-area"]//label[@class="up-label"]')
+            for element in questions_elements:
+                text_content = element.text_content().strip()
+                questions_texts_list.append(text_content)
+            question_fields = page.query_selector_all(f'//div[@class="fe-proposal-job-questions questions-area"]//textarea')
+
+    return questions_texts_list, question_fields
+
+print(pw_check_for_questions())
+
+
+
+def pw_job_posting_apply():
+    pass
