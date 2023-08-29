aa4be10dec9809c77abc9c2df038ec15af20cda2
Plamen Gunchev <plamenmgunchev@gmail.com>
2023-08-29 14:42:55 +0300

    Changes to application function

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, JSHandle
+from typing import List
 from dotenv import load_dotenv
 import os
 import re
 
-
 load_dotenv()
 
-
 # def pw_job_posting():
-
 
 def pw_job_posting_scrape():
 
@@ -31,14 +29,14 @@ def pw_job_posting_scrape():
         available_connects_text = page.query_selector(f'//div[@data-test="connects-auction"]/div[@class="mt-10"]').inner_text()
         available_connects = int(re.findall(r'\d+', available_connects_text)[0])
         posted_on_text = page.query_selector(f'//div[@id="posted-on"]//span[@class]/span').inner_text()
-        client_location_text = page.query_selector(f'//ul[@class = "list-unstyled cfe-ui-job-about-client-visitor m-0-bottom"]/li[@data-qa="client-location"]/strong').inner_text()
+        client_country_text = page.query_selector(f'//ul[@class = "list-unstyled cfe-ui-job-about-client-visitor m-0-bottom"]/li[@data-qa="client-location"]/strong').inner_text()
         page.click('//button[@aria-label="Apply Now"]')
         page.wait_for_event("load")
 
         page.close()
         context.close()
 
-    return description, connects_required, available_connects, posted_on_text, client_location_text
+    return description, connects_required, available_connects, posted_on_text, client_country_text
 
 # print(pw_job_posting_scrape())
 
@@ -51,7 +49,7 @@ def calculate_connects(connects_required: int = 12, available_connects: int = 1)
 job_application = 'https://www.upwork.com/ab/proposals/job/~0174eebd2faa73997b/apply/'
 
 
-def pw_check_for_questions():
+def pw_proposal_fields_to_fill() -> JSHandle | List[str] | List[JSHandle]:
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False, slow_mo=50)
         context = browser.new_context()
@@ -65,19 +63,22 @@ def pw_check_for_questions():
 
         page = context.new_page()
         page.goto('https://www.upwork.com/ab/proposals/job/~01941c52781ae7d491/apply/')
+
+        questions_texts_list = []
         if page.query_selector(f'//div[@class="fe-proposal-job-questions questions-area"]'):
-            questions_texts_list = []
             questions_elements = page.query_selector_all(f'//div[@class="fe-proposal-job-questions questions-area"]//label[@class="up-label"]')
             for element in questions_elements:
                 text_content = element.text_content().strip()
                 questions_texts_list.append(text_content)
             question_fields = page.query_selector_all(f'//div[@class="fe-proposal-job-questions questions-area"]//textarea')
 
-    return questions_texts_list, question_fields
+        cover_letter_field = page.query_selector('//div[@class="cover-letter-area"]//textarea')
 
-print(pw_check_for_questions())
+    return  cover_letter_field, questions_texts_list, question_fields
+
+print(pw_proposal_fields_to_fill())
 
 
 
-def pw_job_posting_apply():
+def pw_proposal_submit():
     pass
