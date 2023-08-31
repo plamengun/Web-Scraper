6023b327052830df0c59b525dfc75cddaa92b15b
Plamen Gunchev <plamenmgunchev@gmail.com>
2023-08-31 09:17:47 +0300

    Synch changes

--- test.py
+++ test.py
@@ -7,37 +7,6 @@ import re
 load_dotenv()
 
 # def pw_job_posting():
-
-def pw_job_posting_scrape():
-
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, slow_mo=50)
-        context = browser.new_context()
-        page = context.new_page()
-        page.goto('https://www.upwork.com/ab/account-security/login')
-        page.fill('input#login_username', os.environ.get('UW_EMAIL'))
-        page.click('button#login_password_continue')
-        page.fill('input#login_password', os.environ.get('UW_PASSWORD'))
-        page.click('button#login_control_continue')
-        page.wait_for_event("load")
-
-        page = context.new_page()
-        page.goto("https://www.upwork.com/jobs/~018842df85fdf63512")
-        description = page.query_selector(f'//div[@data-test="description"]/div').inner_text()
-        connects_required_text = page.query_selector(f'//div[@data-test="connects-auction"]/div').inner_text()
-        connects_required = int(re.findall(r'\d+', connects_required_text)[0])
-        available_connects_text = page.query_selector(f'//div[@data-test="connects-auction"]/div[@class="mt-10"]').inner_text()
-        available_connects = int(re.findall(r'\d+', available_connects_text)[0])
-        posted_on_text = page.query_selector(f'//div[@id="posted-on"]//span[@class]/span').inner_text()
-        client_country_text = page.query_selector(f'//ul[@class = "list-unstyled cfe-ui-job-about-client-visitor m-0-bottom"]/li[@data-qa="client-location"]/strong').inner_text()
-        page.click('//button[@aria-label="Apply Now"]')
-        page.wait_for_event("load")
-
-        page.close()
-        context.close()
-
-    return description, connects_required, available_connects, posted_on_text, client_country_text
-
 # print(pw_job_posting_scrape())
 
 
